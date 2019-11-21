d2de8a99549a2b828b0ab48ec6136a1d32b597c2
Arik Hadas <ahadas@redhat.com>
2019-11-21 16:50:02 +0200

    remove kubevirt.io.V1ListMeta

--- src/main/java/kubevirt/io/V1VirtualMachineInstanceList.java
+++ src/main/java/kubevirt/io/V1VirtualMachineInstanceList.java
@@ -20,12 +20,13 @@ import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+
+import io.kubernetes.client.models.V1ListMeta;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import kubevirt.io.V1ListMeta;
 import kubevirt.io.V1VirtualMachineInstance;
 
 /**

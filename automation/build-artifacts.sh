6609107cc4166938efe49c527863e7f57a331700
Sandro Bonazzola <sbonazzo@redhat.com>
2020-02-28 11:21:02 +0100

    automation: handle releases builds
    build with suffixes during development, drop them for releases.
    
    Change-Id: Iaa29a56812bd3137a736e4b680242bba0b561eb3
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -17,11 +17,24 @@ mv *.tar.gz exported-artifacts/
 yum-builddep java-client-kubevirt.spec
 
 ## build src.rpm
-SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
-rpmbuild \
-    -D "_topdir $PWD/rpmbuild"  \
-    -D "release_suffix ${SUFFIX}" \
-    -ta exported-artifacts/*.gz
+# Is this a release - build from tag?
+if git describe --tags |cut -f2- -d\-| grep -q '-'; then
+    # This is a master build, we want to make every build
+    # newer than all the previous builds using a timestamp,
+    # and make it easy to locate the commit from the build
+    # with the git commit hash.
+    SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+    rpmbuild \
+        -D "_topdir $PWD/rpmbuild"  \
+        -D "release_suffix ${SUFFIX}" \
+        -ta exported-artifacts/*.gz
+else
+    # This is a build from a tagged release, we don't need suffixes.
+    rpmbuild \
+        -D "_topdir $PWD/rpmbuild"  \
+        -ta exported-artifacts/*.gz
+fi
+
 
 find rpmbuild -iname \*.rpm -exec mv {} exported-artifacts/ \;
 

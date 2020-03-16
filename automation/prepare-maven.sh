d48fdc7ef0a7f35ea596029df4c4a59928dcafbc
Artur Socha <asocha@redhat.com>
2020-03-16 12:07:04 +0100

    Fixing upstream, CI, OST
    This patch
    - removes deprecated el7 build support (including toolchain plugin)
    
    - fixes issue with CI releases that did not have generated sources included
      This issue affected engine where after fixing java-client-kubevirt module
      location the OST stopped working [1].
    That happened because kubevirt
    client generated classes were missing and context failed to start.
    Previously the issue was hidden because the module was marked as
    optional so under incorrect location was not auto-discovered
    
    - enables mvn build by default (for config simplicity and consistency)
    
    [1] https://gerrit.ovirt.org/#/c/107378/
    
    Signed-off-by: Artur Socha <asocha@redhat.com>
    Change-Id: I16cddcfb8deab76fe79fa28ca4f1a18964a456a4

--- automation/prepare-maven.sh
+++ automation/prepare-maven.sh
@@ -29,32 +29,3 @@ cat >"$MAVEN_SETTINGS" <<EOS
 </mirrors>
 </settings>
 EOS
-
-
-# set maven 3.5 for el7
-if [[ "$STD_CI_DISTRO" = "el7" ]]; then
-  source /opt/rh/rh-maven35/enable
-
-  # We need to overwrite rh-maven35 configuration for xmvn
-  export XDG_DATA_DIRS="/usr/share:${XDG_DATA_DIRS}"
-
-  XMVN_SETTINGS="/usr/share/xmvn/configuration.xml"
-  mkdir -p "${XMVN_SETTINGS%/*}"
-  cat > "$XMVN_SETTINGS" <<EOS
-<?xml version="1.0" encoding="US-ASCII"?>
-<configuration xmlns="http://fedorahosted.org/xmvn/CONFIG/2.0.0">
-  <repositories>
-    <repository>
-      <id>install</id>
-      <type>compound</type>
-      <configuration>
-        <repositories>
-          <repository>base-install</repository>
-        </repositories>
-      </configuration>
-    </repository>
-  </repositories>
-</configuration>
-EOS
-
-fi
